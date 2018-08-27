3ba5e92793478bdd2d8712b3e493822fd814d6ba
Sayan Riju Chakrabarti <s26c.sayan@gmail.com>
2018-08-27 11:16:58 +0530

    feat: db layer added (Firestore)

---
+++ functions/src/db.ts
@@ -0,0 +1,9 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+
+export { db };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,17 @@
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 import * as functions from 'firebase-functions';
 // import * as firebaseHelper from 'firebase-functions-helper';
 import * as express from 'express';
 import * as bodyParser from "body-parser";
 
-import { app } from "./routes";
+import { app as routes } from "./routes";
 
 const main = express();
 
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
 
-main.use("/api/v1", app);
+main.use("/api/v1", routes);
 
-// webApi is your functions name, and you will pass main as 
+// rest is your functions name, and you will pass main as 
 // a parameter
 export const rest = functions.https.onRequest(main);
