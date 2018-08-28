c9f29596899f2bbf53d94fe2517385e901006430
Sayan Riju Chakrabarti <s26c.sayan@gmail.com>
2018-08-28 15:33:23 +0530

    feat: Output folder changed to dist/ from lib/

--- functions/lib/config.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiVersion = 1;
-//# sourceMappingURL=config.js.map
--- functions/lib/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
-const db = admin.firestore();
-exports.db = db;
-//# sourceMappingURL=db.js.map
